844e2bf2a32c470d715208adc9dddce198cde71c
Marimoraess <moraesmariana2007@gmail.com>
2024-04-02 11:19:02 -0300

    list 2

---
+++ Lista2/1050.php
@@ -0,0 +1,23 @@
+<?php
+
+
+$x = trim(fgets(STDIN));
+
+$ddd = [
+    61 => 'Brasilia',
+    71 => 'Salvador',
+    11 => 'Sao Paulo',
+    21 => 'Rio de Janeiro',
+    32 => 'Juiz de Fora',
+    19 => 'Campinas',
+    27 => 'Vitoria',
+    31 => 'Belo Horizonte',
+];
+
+$return = $ddd[$x] ?? "DDD nao cadastrado";
+
+echo $return."\n";
+
+
+
+?>
